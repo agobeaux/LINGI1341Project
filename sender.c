babc0a5022b45b13bdbd91865a8f114dcfa0f82c
Liliya Semerikova <liliya.semerikova@student.uclouvain.be>
2018-10-15 23:19:01 +0200

    Update sender.c

--- sender.c
+++ sender.c
@@ -38,31 +38,67 @@ const char * real_address(const char *address, struct sockaddr_in6 *rval){
     freeaddrinfo(res);
     return NULL;
 }
-//Creates a socket and initialize it
-int create_socket(struct sockaddr_in6 *dest_addr, int dst_port){
-    
-    int domain = AF_INET6;
-    int type = SOCK_DGRAM;
-    int protocol = IPPROTO_UDP;
-    
-    int sockfd = socket(domain,type,protocol);
-    if(sockfd<0){
-        fprintf(stderr, "problem with socket\n");
+
+/* Creates a socket and initialize it
+ * @source_addr: if !NULL, the source address that should be bound to this socket
+ * @src_port: if >0, the port on which the socket is listening
+ * @dest_addr: if !NULL, the destination address to which the socket should send data
+ * @dst_port: if >0, the destination port to which the socket should be connected
+ * @return: a file descriptor number representing the socket,
+ *         or -1 in case of error (explanation will be printed on stderr)
+ */
+int create_socket(struct sockaddr_in6 *source_addr,
+                  int src_port,
+                  struct sockaddr_in6 *dest_addr,
+                  int dst_port){
+    if(source_addr && src_port > 0 && dest_addr && dst_port > 0){
+        fprintf(stderr, "Both source_addr and dest_addr are available, is it normal ?\n");
         return -1;
     }
-    
-    if(dest_addr!=NULL){
+    struct protoent *proto = getprotobyname("udp");
+    // getprotobyname returns a pointer to a statically allocated protoend structure
+    // FREE PROTO ???
+    if(!proto){
+        fprintf(stderr, "Could not find proto with name \"udp\"\n");
+        return -1;
+    }
+    int sockfd = socket(AF_INET6, SOCK_DGRAM, proto->p_proto);
+    if(sockfd == -1){
+        fprintf(stderr, "Error for socket call : %s\n", strerror(errno)); // perror(); ?
+        return -1;
+    }
+    if(source_addr && src_port > 0){
+        // sin6_port ?... shoudl be in network byte order apparently htons ?
+        // positive int -> transforme into uint32_t. max int 2^31-1
+        // in_port_t similar to uint16_t... not uint32_t...
         
-        if(dst_port>0){dest_addr->sin6_port=htons(dst_port);}
+        // (unsigned short int) host byte order -> network byte order
+        source_addr->sin6_port = htons(src_port);
         
-        int c = connect(sockfd, (const struct sockaddr *)dest_addr,sizeof(struct sockaddr_in6));
-        if(c<0){
-            fprintf(stderr, "problem with connect\n");
+        // source_addr->in_port_t = src_port;
+        if(bind(sockfd, (struct sockaddr *) source_addr, sizeof(struct sockaddr_in6)) == -1){
+            fprintf(stderr, "Error for bind call : %s\n", strerror(errno)); // perror(); ?
+            close(sockfd); //TODO : right ?
             return -1;
         }
+        return sockfd;
     }
-    return sockfd;
+    
+    if(dest_addr && dst_port > 0){
+        dest_addr->sin6_port = htons(dst_port);
+        
+        if(connect(sockfd, (struct sockaddr *) dest_addr, sizeof(struct sockaddr_in6)) == -1){
+            fprintf(stderr, "Error for connect call : %s\n", strerror(errno)); // perror(); ?
+            close(sockfd);
+            return -1;
+        }
+        return sockfd;
+    }
+    close(sockfd); // check at first ?
+    fprintf(stderr, "Couldn't find at a pair (addr, port)\n");
+    return -1;
 }
+
 
 
 
@@ -91,7 +127,7 @@ int main(int argc, char *argv[]){
         switch(opt){
             case 'f':
                 f_option = 1;
-                fd = open(optarg, O_RDWR);
+                fd = open(optarg, O_RDONLY);
                 fprintf(stderr, "Find an f option!\n");
                 if(fd == -1){
                     fprintf(stderr, "Can't open the file!\n");
@@ -106,46 +142,39 @@ int main(int argc, char *argv[]){
     //register hostname && port
     if(f_option==0){
         fprintf(stderr, "I'm in no-f option!\n");
-        res_hostname = argv[0];
-        dst_port = atoi(argv[1]);
-        if(argc>2 && (strcmp(argv[2], "<") == 0)){
-            fd = open(argv[3], O_RDWR);
-            if(fd == -1){
-                fprintf(stderr, "Can't open the file!\n");
-                exit(EXIT_FAILURE);
-            }
-        }
+        res_hostname = argv[1];
+        dst_port = atoi(argv[2]);
         int read = getline(&info, &length, stdin);
-        if (read == -1)
-        {
-            fprintf(stderr, "Error with getline in read_console_input.\n");
-            exit(EXIT_FAILURE);
-        }
-        write(fd, info, sizeof(char)*20);
-        
     }
     else{
         fprintf(stderr, "I'm in f option!\n");
-        res_hostname = argv[3];
-        dst_port = atoi(argv[4]);
+        res_hostname = argv[4];
+        dst_port = atoi(argv[5]);
     }
     
     
     //Resolve the resource name to an usable IPv6 address
-    struct sockaddr_in6 *rval = (struct sockaddr_in6 *)malloc(sizeof(struct sockaddr_in6));
-    const char *check_message = real_address(res_hostname, rval);
-    if(real_address(res_hostname, rval)!=NULL){
+    struct sockaddr_in6 addr;
+    const char *check_message = real_address(res_hostname, &addr);
+    if(check_message){
         fprintf(stderr, "Problem in getaddrinfo %s!\n",check_message);
     }
     
-    //Creates a socket and initialize it
-    socket_fd = create_socket(rval, dst_port);
-    if(socket_fd == -1)exit(EXIT_FAILURE);
+    //Creates a socket and initializes it
+    socket_fd = create_socket(NULL, -1, &addr, dst_port);//réecrire
+    if(socket_fd == -1){
+        fprintf(stderr, "Failed to create the socket!\n");
+        exit(EXIT_FAILURE);
+    }
     
-    const void *buf = "Lily";
-    write(socket_fd, buf, sizeof(*buf));
+    
+    /* DO THINGS */
+    
+    
+    char *buf = "Lily";
+    write(socket_fd,(void *) buf, 5);
     
     close(socket_fd);
     close(fd);
-    free(rval);
+    return 0;
 }
