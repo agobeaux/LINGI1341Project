f94a086ee9b18f92ac139a2706001778129d1459
Liliya Semerikova <liliya.semerikova@student.uclouvain.be>
2018-10-18 14:40:42 +0200

    update sender.c
    Sender can correctly treat, encode and send the information received, but for the moment information cannot exceed one payload (512 bytes).

--- sender.c
+++ sender.c
@@ -1,13 +1,23 @@
 #include "sender.h"
 #include "Q4 INGInious/packet_implem.c"
 #include "Q3 INGInious/real_address.c"
+#include "Q3 INGInious/create_socket.c"
 #include "stack.c"
 
-//TODO Modiefier la structure pour pouvoir faire LIFO -> on ajoute la 1er élément au tete du tableau
-//on retire le plus vieux élement possible
 
 static int size_buffer = 0;
-
+//add  : check if it's OK,if not -> return NULL
+pkt_t *create_packet(char *payload, pkt_t *pkt){
+    //set data
+    pkt_set_type(pkt, PTYPE_DATA);
+    pkt_set_tr(pkt, 0);
+    pkt_set_timestamp(pkt, 0);
+    pkt_set_window(pkt, 1);
+    pkt_set_seqnum(pkt, 0);
+    pkt_set_length(pkt, strlen(payload));
+    pkt_set_payload(pkt, payload, pkt->length);
+    return pkt;
+}
 
 int main(int argc, char *argv[]){
     
@@ -16,9 +26,13 @@ int main(int argc, char *argv[]){
     char *ser_hostname = "::1";
     int dst_port;//port from command line
     int socket_fd;//socket file descriptor
-    size_t length = sizeof(char)*40;
-    char *info = (char *)malloc(length); // !!!!!!!!not sure of the value
-    
+    int can_read = 1;//can continue to read from file
+    int f_option = 0;//if there is (not) f_option
+    char *new_payload=(char *)malloc(MAX_PAYLOAD_SIZE);
+    int err;
+    int window_size=5;//for this moment window is fixe and equal to 5
+    size_t len = 528;
+    char *buf = (char*)malloc(528);
     
     
     //check if there is enough arguments to continue
@@ -29,7 +43,6 @@ int main(int argc, char *argv[]){
     
     //register file if there are an -f option
     int opt;
-    int f_option = 0;
     while ((opt = getopt(argc, argv, "f:")) != -1){
         switch(opt){
             case 'f':
@@ -51,16 +64,16 @@ int main(int argc, char *argv[]){
         fprintf(stderr, "I'm in no-f option!\n");
         res_hostname = argv[1];
         dst_port = atoi(argv[2]);
-        int read = getline(&info, &length, stdin);
-        if(read==-1){
-            fprintf(stderr, "Problem in geting line stdin!\n");
-        }
-        fprintf(stderr, "Info we are getting from stdin %s!\n", info);
+        //int read = getline(&info, &length, stdin);
+        //if(read==-1){
+           // fprintf(stderr, "Problem in geting line stdin!\n");
+        //}
+        //fprintf(stderr, "Info we are getting from stdin %s!\n", info);
     }
     else{
         fprintf(stderr, "I'm in f option!\n");
-        res_hostname = argv[4];
-        dst_port = atoi(argv[5]);
+        res_hostname = argv[3];
+        dst_port = atoi(argv[4]);
     }
     
     
@@ -86,19 +99,51 @@ int main(int argc, char *argv[]){
     
     
     /* DO THINGS */
-    //envoyer les packets
-    //traiter les ack
-    //traiter les nack
-    //appel bloquant pour le sender, quand il arrete à read? avec quoi?
+    //faire buffer pour plusieurs payload
     //window taille fixe
     //retransmission timer
-    //
+    //implementation of while
+    //while(true){
+        //lire dans le fichier
     
     
-    char *buf = "Lily";
-    write(socket_fd,(void *) buf, 5);
+    //pour le moment traiter qu'un seul payload reçu
+        if(can_read){
+            //if we have a file
+            if(f_option){
+                err = read(fd, (void *)new_payload, MAX_PAYLOAD_SIZE);
+                if(err == -1){
+                    fprintf(stderr, "Failed to read a new payload!\n");
+                    exit(EXIT_FAILURE);
+                }
+            }
+            //if we must read from stdin
+            else{
+                err = read(0, (void *)new_payload, MAX_PAYLOAD_SIZE);
+                if(err == -1){
+                    fprintf(stderr, "Failed to read a new payload!\n");
+                    exit(EXIT_FAILURE);
+                }
+            }
+            //when we have a payload, create a packet
+            pkt_t* pkt = pkt_new();
+            pkt = create_packet(new_payload, pkt);
+            pkt_encode(pkt, buf, &len);
+            write(socket_fd,(void *) buf, &len + 16);
+        }
+    
+    //}
+    
+    
+    //TODO
+    //traitement de signal, reception de ack, nack, traitement de signal
+    //appel bloquant pour le sender, quand il arrete à read? avec quoi?
+    
+    
     
     close(socket_fd);
     close(fd);
+    free(new_payload);
+    free(buf);
     return 0;
 }
